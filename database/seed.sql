0b7bb472e0da03a8b4f8515ea1d05117e85d599d
gunkoo <sayapekerjaan72@gmail.com>
2026-08-09 03:00:43 +0700

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,8 +1,4 @@
--- ============================================================
--- SEED DATA: Initial Superadmin account
--- Password default: 270900 (dalam Base64: MjcwOTAw)
--- Ganti password setelah deploy pertama!
--- ============================================================
+DELETE FROM users WHERE username = 'SUPERADMIN';
 
 INSERT OR IGNORE INTO users (username, password, nama, role, active, created_at, masa_aktif_hari)
-VALUES ('SUPERADMIN', 'MjcwOTAw', 'Muhammad Eldhi', 'superadmin', 1, datetime('now'), 9999);
+VALUES ('ELDHI', 'MjcwOTAw', 'Muhammad Eldhi', 'superadmin', 1, datetime('now'), 9999);
